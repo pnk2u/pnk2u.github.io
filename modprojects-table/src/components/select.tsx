89c0fc169feafb700a9f894c9d207310d0f9ea35
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-04-11 04:36:04 +0200

    Communicate theme colours in dropdown ui

--- modprojects-table/src/components/select.tsx
+++ modprojects-table/src/components/select.tsx
@@ -16,10 +16,10 @@ const Select: Component<props> = (props) => {
   const [open, setOpen] = createSignal(false);
 
   return (
-    <div class="relative text-fore-100 text-lg">
+    <div class="relative z-1 text-fore-100 text-lg">
       <button
         onClick={() => setOpen((prev) => !prev)}
-        class="flex items-center p-2 gap-2 cursor-pointer rounded-sm bg-base-700"
+        class="flex items-center p-2 gap-2 cursor-pointer rounded-sm bg-base-600/85"
       >
         {props.options
           .find((option) => option.value === props.value())
@@ -28,13 +28,13 @@ const Select: Component<props> = (props) => {
       </button>
       <div
         class={
-          "absolute flex flex-col items-center bg-base-700 p-2 gap-1 rounded-sm top-[calc(100%+(var(--spacing)*1.5))] w-full " +
+          "absolute flex flex-col items-center bg-base-600/85 p-2 gap-1 rounded-sm top-[calc(100%+(var(--spacing)*1.5))] w-full " +
           (open() ? "visible" : "invisible")
         }
       >
         <For each={props.options}>
           {(option) => {
-            const isSelected = () => option.value === props.value();
+            const isTheme = (value: string) => option.value === value;
             return (
               <button
                 onClick={() => {
@@ -42,8 +42,12 @@ const Select: Component<props> = (props) => {
                   setOpen((prev) => !prev);
                 }}
                 class={
-                  "hover:bg-base-900 hover:text-fore-300 p-2 cursor-pointer rounded-sm " +
-                  (isSelected() ? "bg-base-600" : "")
+                  "hover:bg-base-700/85 hover:text-fore-300 p-2 cursor-pointer rounded-sm " +
+                  (isTheme(props.value()) ? "bg-base-500" :
+                      isTheme("lieon") ? "bg-themet3-base/75 text-themet3-fore hover:bg-themet3-base hover:text-themet3-acct" :
+                          isTheme("dark") ? "bg-themet2-base/75 text-themet2-fore hover:bg-themet2-base hover:text-themet2-acct":
+                              isTheme("light") ? "bg-themet1-base/75 text-themet1-fore hover:bg-themet1-base hover:text-themet1-acct"
+                                : "")
                 }
               >
                 {option.display()}
